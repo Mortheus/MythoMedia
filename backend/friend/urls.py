766ce53f65f82a8161502e352b258aafe8cd83d1
DESKTOP-UQ9KTPD\crist <cristea.andrei.199@gmail.com>
2023-05-18 00:13:03 -0700

    Added blocked user functionality.

--- backend/friend/urls.py
+++ backend/friend/urls.py
@@ -3,13 +3,17 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 from backend.friend.views import ShowFriendsView, ShowFriendRequestsView, SendFriendRequestView, \
-    HandleFriendRequestView, GetSuggestedFriendsView
+    HandleFriendRequestView, GetSuggestedFriendsView, GetBlockedUsers, BlockUser, UnblockUser, FullBlock
 
 urlpatterns = [
     path("", ShowFriendsView.as_view()),
     path("/requests", ShowFriendRequestsView.as_view()),
-    path("/add-friend", SendFriendRequestView.as_view()),
+    path("/add-friend/<str:username>", SendFriendRequestView.as_view()),
     path("/request/<str:username>", HandleFriendRequestView.as_view()),
     path("/suggestions", GetSuggestedFriendsView.as_view()),
+    path("/blocked_users", GetBlockedUsers.as_view()),
+    path("/block_user/<str:username>", BlockUser.as_view()),
+    path("/unblock_user/<str:username>", UnblockUser.as_view()),
+    path("/full_block/<str:username>", FullBlock.as_view()),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
